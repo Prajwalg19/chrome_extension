e2b237e16bec1617edc4d7adb0f2391ad2b75e1d
nova <theimperical8@gmail.com>
2022-05-31 19:36:06 +0530

    black_jack game

--- letscount.js
+++ letscount.js
@@ -1,8 +1,32 @@
-let count = 0;
-increment = () => {
-	document.getElementById("count").innerHTML = ++count;
+let firstCard = 0;
+let secondCard = 16;
+sum = firstCard + secondCard;
+arr = [firstCard, secondCard];
+let answer;
+let card;
+ya = document.getElementById('Cards')
+yo = document.getElementById('Sum')
+les = document.getElementById("result");
+start_game = () => {
+	render_game();
 };
-save = () => {
-	document.getElementById("count").innerHTML = 0;
-	return (count = 0);
+
+render_game = () => {
+
+	ya.textContent ="Cards :" + arr;
+	yo.innerHTML ="Sum : " + sum;
+	if (sum < 21) {
+		les.innerHTML = "do u want to draw ";
+	} else if (sum === 21) {
+		les.innerHTML = "you won!!";
+	} else {
+		les.innerHTML = "you lost ";
+	}
 };
+function newcard() {
+	document.querySelector(".card_drawn").innerHTML = "Drawing a new card";
+	card = 1;
+	arr.push(card);
+	sum += card;
+	render_game();
+}
